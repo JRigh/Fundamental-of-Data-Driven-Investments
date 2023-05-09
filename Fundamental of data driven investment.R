28893ba617d7238f87d4719bedb2a4628869b7ba
Julian Sampedro <julian.righ.sampedro@gmail.com>
2023-05-09 23:49:33 +0200

    Add files via upload

---
+++ Fundamental of data driven investment.R
@@ -0,0 +1,22 @@
+#---------------------------------------
+# Fundamental of Data-driven investments
+#---------------------------------------
+
+# lecture 1
+
+getwd()
+setwd("C:/Users/yjcla/Documents/R test")
+
+#data folder 
+DIS <- read.csv(file = "C:/Users/yjcla/Documents/R test/Data/DIS.csv")
+DIS <- read.csv(file = "./Data/DIS - Copy.csv")
+#DIS <- read.csv(file = "./Data/DIS.csv", header = FALSE)
+
+
+is(DIS$Date)
+DIS$date.format <- as.Date(as.character(DIS$Date), format="%Y-%m-%d")
+DIS$date.format <- as.Date(as.character(DIS$Date), format="%m/%d/%Y")
+
+is(DIS$date.format)
+
+save(DIS, file = "./RData/DIS.RData")
